f290a303e3889c73370dd87e6773fd2c88e5e72f
Jie Zhou <zhoujie338@126.com>
2016-01-07 15:22:04 +0800

    add scripts

---
+++ package/scripts/impala-catalog.py
@@ -0,0 +1,53 @@
+import sys, os, pwd, signal, time
+from resource_management import *
+from resource_management.core.base import Fail
+from resource_management.core.exceptions import ComponentIsNotRunning
+from subprocess import call
+
+
+class ImpalaCatalog(Script):
+    #Call setup.sh to install the service
+    def install(self, env):
+
+        # Install packages listed in metainfo.xml
+        self.install_packages(env)
+
+        cmd = 'yum-config-manager --add-repo  ' \
+              'http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/cloudera-cdh5.repo'
+
+        Execute('echo "Running ' + cmd + '"')
+        Execute(cmd)
+
+
+        cmd = 'yum install  impala-server impala-catalog impala-state-store impala-shell'
+        Execute('echo "Running ' + cmd + '"')
+        Execute(cmd)
+
+        self.configure(env)
+
+    def configure(self, env):
+        import params
+
+        env.set_params(params)
+
+
+    #Call start.sh to start the service
+    def start(self, env):
+        cmd = 'service impala-catalog start'
+        Execute('echo "Running cmd: ' + cmd + '"')
+        Execute(cmd)
+
+    #Called to stop the service using the pidfile
+    def stop(self, env):
+        cmd = 'service impala-catalog stop'
+        Execute('echo "Running cmd: ' + cmd + '"')
+        Execute(cmd)
+
+    #Called to get status of the service using the pidfile
+    def status(self, env):
+        cmd = 'service impala-catalog status'
+        Execute('echo "Running cmd: ' + cmd + '"')
+        Execute(cmd)
+
+if __name__ == "__main__":
+    ImpalaCatalog().execute()
--- package/scripts/impala-daemon.py
+++ package/scripts/impala-daemon.py
@@ -5,7 +5,7 @@ from resource_management.core.exceptions import ComponentIsNotRunning
 from subprocess import call
 
 
-class StateStore(Script):
+class ImpalaDaemon(Script):
     #Call setup.sh to install the service
     def install(self, env):
 
@@ -17,6 +17,7 @@ class StateStore(Script):
 
         Execute('echo "Running ' + cmd + '"')
         Execute(cmd)
+
 
         cmd = 'yum install  impala-server impala-catalog impala-state-store impala-shell'
         Execute('echo "Running ' + cmd + '"')
@@ -32,31 +33,21 @@ class StateStore(Script):
 
     #Call start.sh to start the service
     def start(self, env):
-        import params
-        self.set_conf_bin(env)
-        self.configure(env)
-
-        self.create_hdfs_user(params.flink_user)
-        cmd = 'service impala-state-store start'
+        cmd = 'service impala-server start'
         Execute('echo "Running cmd: ' + cmd + '"')
         Execute(cmd)
 
     #Called to stop the service using the pidfile
     def stop(self, env):
-        cmd = 'service impala-state-store stop'
+        cmd = 'service impala-server stop'
         Execute('echo "Running cmd: ' + cmd + '"')
         Execute(cmd)
 
     #Called to get status of the service using the pidfile
     def status(self, env):
-        cmd = 'service impala-state-store status'
+        cmd = 'service impala-server status'
         Execute('echo "Running cmd: ' + cmd + '"')
         Execute(cmd)
 
-    def create_hdfs_user(self, user):
-        Execute('hadoop fs -mkdir -p /user/'+user, user='hdfs', ignore_failures=True)
-        Execute('hadoop fs -chown ' + user + ' /user/'+user, user='hdfs')
-        Execute('hadoop fs -chgrp ' + user + ' /user/'+user, user='hdfs')
-
 if __name__ == "__main__":
-    StateStore().execute()
+    ImpalaDaemon().execute()
